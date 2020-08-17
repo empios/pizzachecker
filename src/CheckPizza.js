8ee87f3b2132720b711bad837805061f22977c6c
EmpioS <pawel.wlo@wp.pl>
2020-08-17 23:41:24 +0200

    -Fixed fetching and showing info

--- src/CheckPizza.js
+++ src/CheckPizza.js
@@ -6,6 +6,7 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 
 
 export default class CheckPizza extends React.Component {
@@ -20,76 +21,108 @@ export default class CheckPizza extends React.Component {
         restaurants: [],
         price: [],
         chosenRestaurant: null,
-        valueOfChosenRestaurant: null
+        valueOfChosenRestaurant: 0
     }
-
 
 
     componentDidMount() {
         axios.get("https://cors-anywhere.herokuapp.com/https://elblagpizzaapi.herokuapp.com/").then(response => {
                 this.setState({price: response.data})
-                console.log(this.state.price)
             }
         )
         axios.get("https://cors-anywhere.herokuapp.com/https://elblagpizzaapi.herokuapp.com/restaurant").then(response => {
             this.setState({restaurants: response.data.restaurant})
-            console.log(this.state.restaurants)
         })
     }
 
-    handleClick(event) {
-        this.setState({chosenRestaurant: event.target.value})
-        this.setState({valueOfChosenRestaurant: this.state.price.pizzapricebysurface[this.state.chosenRestaurant] })
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.chosenRestaurant !== this.state.chosenRestaurant) {
+            this.setState({valueOfChosenRestaurant: this.state.price.pizzapricebysurface[this.state.chosenRestaurant]})
+        }
     }
 
 
+    handleClick(event) {
+        this.setState({chosenRestaurant: event.target.value})
+    }
+
+
+
+
     render() {
-        return (
-            <Grid
-                container
-                direction="column"
-                justify="center"
-                alignContent="center"
-                spacing={3}
-                style={{minHeight: '50vh'}}
-            >
+        let comp;
+        if (this.state.valueOfChosenRestaurant === 0){
+            comp = "Wybierz restauracje"
+        }
+        if (this.state.valueOfChosenRestaurant !== 0) {
+            comp = this.state.valueOfChosenRestaurant
+        }
+        if (this.state.restaurants.length > 0) {
+            return (
 
-                <Grid item xl={3} sm={12}>
-                    <h2 style={{textAlign: "center"}}>Wybierz restaurację i zobacz jej cenę za 1cm<sup>3</sup></h2>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignContent="center"
+                    spacing={3}
+                    style={{minHeight: '50vh'}}
+                >
+
+                    <Grid item xl={3} sm={12}>
+                        <h2 style={{textAlign: "center"}}>Wybierz restaurację i zobacz jej cenę za 1cm<sup>3</sup></h2>
+
+                    </Grid>
+                    <Grid item xl={3} sm={12} style={{textAlign: "center"}}>
+                        <FormControl>
+                            <InputLabel style={{color: "white"}}
+                                        id="demo-simple-select-helper-label">Restauracja</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-helper-label"
+                                id="demo-simple-select-helper"
+                                value={this.state.chosenRestaurant}
+                                onChange={this.handleClick}
+                                style={{color: "white", alignContent: "center"}}
+                            >
+                                {
+                                    this.state.restaurants.map((restaurant, index) =>
+                                        <MenuItem key={index} value={index}>
+                                            {restaurant}
+                                        </MenuItem>
+                                    )}
+                            </Select>
+                            <FormHelperText style={{color: "white"}}>Wybierz restaurację z której chcesz zamówić
+                                pizzę</FormHelperText>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xl={3} sm={12} style={{textAlign: "center"}}>
+                    </Grid>
+
+                    <Grid item xl={3} sm={12}>
+                        <h4 id="getelement" style={{textAlign: "center"}}>{comp}</h4>
+                    </Grid>
 
                 </Grid>
+            )
+        } else {
+            return (
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignContent="center"
+                    spacing={3}
+                    style={{minHeight: '50vh'}}
+                >
 
-                <Grid item xl={3} sm={12} style={{textAlign: "center"}}>
-                    <FormControl>
-                        <InputLabel style={{color: "white"}}
-                                    id="demo-simple-select-helper-label">Restauracja</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-helper-label"
-                            id="demo-simple-select-helper"
-                            value={this.state.chosenRestaurant}
-                            onChange={this.handleClick}
-                            style={{color: "white", alignContent: "center"}}
-                        >
-                            {
-                                this.state.restaurants.map((restaurant, index) =>
-                                    <MenuItem key={index} value={index}>
-                                        {restaurant}
-                                    </MenuItem>
-                                )}
-                        </Select>
-                        <FormHelperText style={{color: "white"}}>Wybierz restaurację z której chcesz zamówić
-                            pizzę</FormHelperText>
-                    </FormControl>
-
+                    <Grid item xl={3} sm={12}>
+                        <h2 style={{textAlign: "center"}}>Pobieram informacje...</h2>
+                    </Grid>
                 </Grid>
-                <Grid item xl={3} sm={12} style={{textAlign: "center"}}>
+            )
 
-                    <h3 id="getelement">{this.state.valueOfChosenRestaurant}</h3>
+        }
 
-
-                </Grid>
-
-            </Grid>
-        )
     }
 }
